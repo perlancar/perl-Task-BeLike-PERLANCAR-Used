c6eb21a176da51b3b901cd0fd6f495057748fdb6
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-07-07 13:32:05 +0700

    [doc] Add Synopsis

--- lib/Task/BeLike/PERLANCAR/Used.pm
+++ lib/Task/BeLike/PERLANCAR/Used.pm
@@ -9,6 +9,15 @@ package Task::BeLike::PERLANCAR::Used;
 1;
 # ABSTRACT: All my modules which I currently use and install on a new perl installation
 
+=head1 SYNOPSIS
+
+Here's how I usually bootstrap a fresh perl installation (e.g. after a
+C<perlbrew install>):
+
+ % cpanm -n App::cpanm::perlancar
+ % cpanm-perlancar -n Task::BeLike::PERLANCAR::Used
+
+
 =pkggroup Included modules
 
 #CODE: require App::PMVersionsUtils; for (`bash -c 'setop --diff <(lcpan author-dists PERLANCAR | dist2mod) <(cpanmodules ls-entries PERLANCAR::Unused)'`) { chomp; my $res = App::PMVersionsUtils::version_from_pmversions(module=>$_); die "Error when checking minimum version for $_: $res->[0] - $res->[1]" unless $res->[0] == 200; my $v = $res->[2] || 0; print "=pkg $_ $v\n\n" }
